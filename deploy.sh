b6cdd949876de6b019f41e1295900cedf798283a
joshua1988 <jangkeehyo@gmail.com>
2018-08-24 20:15:05 +0900

    Updated the shell script to deploy on github page

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,22 @@
 # abort on errors
 set -e
 
-# build
-npm run build
-
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
+git init
+git add -A
+git commit -m 'deploy with vuepress script'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/joshua1988/vue-camp.git master:gh-pages
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git checkout gh-pages
-git add -A
-git commit -m 'deploy'
-git push -f origin gh-pages
-git checkout master
+# git checkout gh-pages
+# git add -A
+# git commit -m 'deploy'
+# git push -f origin gh-pages
+# git checkout master
 
 cd -
