02add827db426033df0c6446ccb56d99137894b9
joshua1988 <jangkeehyo@gmail.com>
2018-09-07 19:56:36 +0900

    add controlled component sample

---
+++ 5_design-pattern/controlled/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <check-box v-model="checked"></check-box>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      checked: false,
+    }
+  }
+}
+</script>
---
+++ 5_design-pattern/controlled/src/components/CheckBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="checkbox" :value="value" @click="toggle">
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    toggle() {
+      this.$emit('input', !this.value);
+    }
+  }
+}
+</script>
