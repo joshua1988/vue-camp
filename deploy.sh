d6ff2c8ba119976c5216d6d707dd3dce43c985d9
joshua1988 <jangkeehyo@gmail.com>
2018-09-13 18:48:34 +0900

    Add a quiz for reactivity

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy with vuepress script'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/joshua1988/vue-camp.git master:gh-pages
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-# git checkout gh-pages
-# git add -A
-# git commit -m 'deploy'
-# git push -f origin gh-pages
-# git checkout master
 
 cd -
