44fc298e48c23b548902643ebdc40dbcbd3a96e3
joshua1988 <jangkeehyo@gmail.com>
2018-09-22 18:39:18 +0900

    add data provider sample

---
+++ 5_design-pattern/data-provider/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-md mx-auto">
+      <fetch-data url="contacts.json" class="mb-8">
+        <div class="card" slot-scope="{ response: contacts, loading }">
+          <h1 class="text-2xl font-bold mb-6">Your Contacts</h1>
+          <div>
+            <div v-for="contact in contacts" :key="contact.id" class="flex items-center spaced-y-6">
+              <img :src="contact.picture.medium" class="h-12 w-12 rounded-full block mr-2" alt="">
+              <div>
+                <div class="font-bold">
+                  {{ contact.name.first }} {{ contact.name.last }}
+                </div>
+                <div class="text-grey-dark">
+                  {{ contact.email }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </fetch-data>
+    </div>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FetchData
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"/>
---
+++ 5_design-pattern/data-provider/src/components/FetchData.vue
@@ -0,0 +1,31 @@
+<script>
+/* eslint-disable */
+import axios from 'axios';
+
+export default {
+  props: ['url'],
+  data() {
+    return {
+      response: null,
+      loading: false,
+    }
+  },
+  created() {
+    axios.get(this.url)
+      .then(response => {
+        this.response = response.data;
+        this.loading = false;
+      })
+      .catch(error => {
+        alert('[ERROR] fetching the data', error);
+        console.log(error);
+      });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading,
+    });
+  },
+}
+</script>
