cf3039620e3eb05cbf9184f23323005d828f41d7
joshua1988 <jangkeehyo@gmail.com>
2018-10-05 17:45:16 +0900

    add answers for es6

---
+++ 2_todo/es6/modules/answer/js/app.js
@@ -0,0 +1,6 @@
+// 할일 #1 - math.js 파일에서 두 숫자의 합을 구하는 sum 함수를 임포트하여 콘솔에 찍어보세요.
+// 문법 참고 : import 불러올 기능 or 변수 from '파일의 위치'
+import { sum, num } from './math.js';
+
+console.log(sum());
+console.log(num);
---
+++ 2_todo/es6/modules/answer/js/math.js
@@ -0,0 +1,6 @@
+// sum() 함수 구현 후 export 하세요.
+export function sum(a, b) {
+  return a + b;
+}
+
+export var num = 10;
