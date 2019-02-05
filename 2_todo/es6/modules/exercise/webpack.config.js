674457931c22397c6a3fa34ebcd0d48a1e3fc435
joshua1988 <jangkeehyo@gmail.com>
2019-02-05 13:31:52 +0900

    예제 수정

--- 2_todo/es6/modules/exercise/webpack.config.js
+++ 2_todo/es6/modules/exercise/webpack.config.js
@@ -1,25 +1,26 @@
 var path = require('path');
 
 module.exports = {
-		mode: 'none',
-		entry: './js/app.js',
-		output: {
-				path: path.resolve(__dirname, 'build'),
-				filename: 'main.bundle.js'
-		},
-		module: {
-				rules: [
-						{
-								test: /\.js$/,
-								loader: 'babel-loader',
-								query: {
-										presets: ['es2015']
-								}
-						}
-				]
-		},
-		stats: {
-				colors: true
-		},
-		devtool: 'source-map'
+	mode: 'none',
+	entry: './js/app.js',
+	output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'main.bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.m?js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		}]
+	},
+	stats: {
+			colors: true
+	},
+	devtool: 'source-map'
 };
