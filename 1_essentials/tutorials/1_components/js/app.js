d45dc298650ce946a10701297ace50afa2c2e011
joshua1988 <jangkeehyo@gmail.com>
2019-06-29 15:29:02 +0900

    1주차 수업 내용

--- 1_essentials/tutorials/1_components/js/app.js
+++ 1_essentials/tutorials/1_components/js/app.js
@@ -3,9 +3,18 @@ Vue.component('global-component', {
   template: '<p><strong>This is a global component</strong></p>'
 });
 
+// 실습 #1 - `another-global-component` 컴포넌트 전역 등록
+// <p>This is another global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+Vue.component('another-global-component', {
+  template: '<p>This is another global component</p>'
+});
+
 // 지역 컴포넌트 내용
 var cmp = {
   template: '<p><strong>This is a local component</strong></p>'
+}
+var cmp2 = {
+  template: '<p>This is another local component</p>'
 }
 
 var app = new Vue({
@@ -15,12 +24,11 @@ var app = new Vue({
   },
   // 지역 컴포넌트 등록
   components: {
-    'local-component' : cmp
+    'local-component' : cmp,
+    'another-local-component': cmp2,
   },
 
   // 실습 #2 - `another-local-component` 컴포넌트 지역 등록
   // <p>This is another local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 });
 
-// 실습 #1 - `another-global-component` 컴포넌트 전역 등록
-// <p>This is another global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
