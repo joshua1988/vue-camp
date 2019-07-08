ee768bbd18a1703085835eed65fbf6636e2bd073
joshua1988 <jangkeehyo@gmail.com>
2019-07-08 20:49:13 +0900

    5회차 1교시 수업 코드

---
+++ 3_vuex/slots/src/components/ListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li v-bind:style="textStyle">
+    <!-- #1 -->
+    {{ item }}
+    <!-- #2 -->
+    <p>{{ item }}</p>
+    <img src="" alt="">
+  </li>
+</template>
+
+<script>
+export default {
+	props: ['item', 'textStyle'],
+};
+</script>
+
+<style>
+</style>
---
+++ 3_vuex/slots/src/components/SlotListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <!-- 컴포넌트 태그를 등록하는 시점에 넣는 코드는 다 아래로 옵니다 -->
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
