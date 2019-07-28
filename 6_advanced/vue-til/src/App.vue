7ff860228d54d2f751b9bb1d7beedd8fd4284191
Jay Jin <jayjinjay@gmail.com>
2019-07-28 21:05:01 +0900

    Add footer

--- 6_advanced/vue-til/src/App.vue
+++ 6_advanced/vue-til/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="app">
     <app-header></app-header>
-    <router-view></router-view>
+    <div class="app-contents">
+      <router-view></router-view>
+    </div>
+    <app-footer></app-footer>
     <toast-popup></toast-popup>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/common/AppHeader.vue';
+import AppFooter from './components/common/AppFooter.vue';
 import ToastPopup from './components/common/ToastPopup.vue';
 
 export default {
   components: {
     AppHeader,
     ToastPopup,
+    AppFooter,
   },
 }
 </script>
