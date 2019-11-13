6916a941d118ac168542ad6449285cbaed46b1b6
CaptainPangyo <jangkeehyo@gmail.com>
2019-11-13 23:02:02 +0900

    7회차 3교시 코드

---
+++ 4_spa/vue-chart/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- TODO: BarChart 컴포넌트화 -->
+    <bar-chart v-bind:chartData="barChartData"></bar-chart>
+    <!-- TODO: LineChart 컴포넌트화: Getting Started에 코드 있음-->
+    <!-- NOTE: vim, vue와 같은 단축키 활용해보세요..! ES6 문법도..! -->
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+import BarChart from "@/components/BarChart.vue";
+import LineChart from "@/components/LineChart.vue";
+
+export default {
+  components: {
+    BarChart,
+    LineChart
+  },
+  data() {
+    return {
+      barChartData: []
+    };
+  },
+  methods: {
+    fetchChartData() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          var result = [12, 19, 3, 5, 2, 3];
+          resolve(result);
+        }, 3000);
+      });
+    }
+  },
+  created() {
+    this.fetchChartData().then(data => {
+      console.log("받아왔음");
+      this.barChartData = data;
+    });
+  }
+
+  // beforeMount() {
+  //   `<div>
+  //     <canvas id="myChart" width="400" height="400"></canvas>
+  //   </div>`
+  //   document.querySelector('canvas')
+  //   $('canvas');
+  // },
+  // mounted() {
+  //   <div>
+  //     <canvas id="myChart" width="400" height="400"></canvas>
+  //   </div>
+  // },
+  // updated() {},
+  // beforeDestroy() {}
+};
+</script>
+
+<style></style>
---
+++ 4_spa/vue-chart/src/components/BarChart.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <canvas ref="myChart" id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["chartData"],
+  watch: {
+    chartData() {
+      this.renderChart();
+    }
+  },
+  methods: {
+    renderChart() {
+      // console.log(document.getElementById("myChart"));
+      // console.log(this.$refs.myChart);
+      var ctx = this.$refs.myChart.getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: this.chartData,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        },
+        events: {
+          // clickTooltip() {
+          //   this.$emit('click:tootip')
+          // }
+        }
+      });
+    }
+  },
+  mounted() {
+    this.renderChart();
+  }
+};
+</script>
+
+<style></style>
