ebfd013c658d7d30b251846073d0cbf12994ad32
khnoh <noh5524@gmail.com>
2021-09-09 21:21:53 +0900

    [tool] prettier

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ["meta", { name: 'theme-color', content: '#3eaf7c' }],
     ["meta", { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ["meta", { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ["link", { rel: 'apple-touch-icon', href: '/images/icons/152x.png' }],
