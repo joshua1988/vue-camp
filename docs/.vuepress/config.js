9403a3013012f4f0018e3faa6bf9740adf69d277
Daye Lee <leeda1801@gmail.com>
2021-10-08 22:04:45 +0900

    [스크롤 효과] 부드러운 페이지 스크롤 효과 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
     docsRepo: 'joshua1988/vue-camp',
     docsDir: 'docs',
     editLinks: true,
+    smoothScroll: true,
     locales: {
       '/': {
         selectText: '언어',
